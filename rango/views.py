0b47154a629b51e32e6e871f6ecd215f9506ee5c
saeed alshammar <2840464a@student.gla.ac.uk>
2024-02-08 22:53:23 +0000

    chapter 8

--- rango/views.py
+++ rango/views.py
@@ -21,7 +21,11 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page <a href='/rango/'>Index</a>")
+    print(request.method)
+
+    print(request.user)
+
+    return render(request, 'rango/about.html',{})
 
 def show_category(request, category_name_slug):
     context_dict ={}
